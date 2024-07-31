9bbcfcf19a2dd0857656bb1088926ac2f1f06be8
Aman verma <vermaaman.1011@gmail.com>
2024-07-31 14:43:16 +0530

    Learned variables

---
+++ variables.js
@@ -0,0 +1,11 @@
+const accountID = 12345678
+let emailID = "aman1111@gmail.com"
+var accountpassword = "11111"
+accountcity = "potheri"
+
+emailID = "verma1111@gmail.com"
+accountpassword = "22222"
+accountcity = "chennai"
+
+console.log(accountID);
+console.table([accountID, emailID, accountpassword, accountcity]) 
